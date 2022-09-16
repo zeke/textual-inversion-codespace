9049b6c3c8aab6257a838104836da629334584cf
Chenxi <chenxi.whitehouse@gmail.com>
2022-09-16 13:44:52 +0100

    Update inference.py

--- inference.py
+++ inference.py
@@ -1,12 +1,24 @@
+import argparse
 import torch
 from diffusers import StableDiffusionPipeline
 
-model_id = "textual_inversion_spyro-dragon"
-pipe = StableDiffusionPipeline.from_pretrained(model_id,torch_dtype=torch.float16).to("cuda")
 
-prompt = "A cat in the style of <spyro-dragon>."
+if __name__ == "__main__":
+    
+    parser = argparse.ArgumentParser()
+    
+    parser.add_argument(
+        "--prompt",
+        default="Golden Gate Bridge in style of <spyro-dragon>.",
+    )
+    parser.add_argument(
+        "--output",
+        default="golden.png",
+    )
+    args = parser.parse_args()
+    model_id = "textual_inversion_spyro-dragon"
+    pipe = StableDiffusionPipeline.from_pretrained(model_id,torch_dtype=torch.float16).to("cuda")
+    with torch.autocast("cuda"):
+        image = pipe(args.prompt, num_inference_steps=50, guidance_scale=7.5).images[0]
 
-with torch.autocast("cuda"):
-    image = pipe(prompt, num_inference_steps=50, guidance_scale=7.5).images[0]
-
-image.save("cat-dragon.png")
+    image.save(args.output)
